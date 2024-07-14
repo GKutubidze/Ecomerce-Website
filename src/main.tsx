d33afcfbaa51bb04da5f5fc686d3a2cd97d7efcd
Giorgi Kutubidze <ffs@gmail.com>
2024-07-14 22:57:34 +0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,14 @@ import Loading from "./components/Loading/Loading";
 import { CountryProvider } from "./context/CountryContext";
 import { CartProvider } from "./context/CartContext";
 import { ProductProvider } from "./context/ProductContext";
-import SpeedInsightsProvider from "./context/SpeedInsightsWrapper";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <CartProvider>
         <ProductProvider>
           <CountryProvider>
-            <SpeedInsightsProvider>
-              <Loading />
-              <RouterProvider router={router} />
-            </SpeedInsightsProvider>
+            <Loading />
+            <RouterProvider router={router} />
           </CountryProvider>
         </ProductProvider>
       </CartProvider>
