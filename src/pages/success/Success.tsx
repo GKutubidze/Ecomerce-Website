2bea89b3ee6d197247fe0bb7d9f86e97c3537dfd
Giorgi Kutubidze <kutubidzegiorgi@gmail.com>
2024-11-30 01:39:27 +0400

    fix stripe

--- src/pages/success/Success.tsx
+++ src/pages/success/Success.tsx
@@ -17,7 +17,7 @@ const Success = () => {
     const verifyPayment = async (sessionId: string) => {
       try {
         // Fetch session to confirm payment
-        const response = await fetch(`http://localhost:5000/verify-session`, {
+        const response = await fetch(`https://ecomerce-website-blue.vercel.app/verify-session`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
